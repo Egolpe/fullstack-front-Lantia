1fcb725d2eb1a08613be49de55b9f2230617cae3
Enrique <enriquegolpe@gmail.com>
2020-08-08 21:31:32 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { PrivateRoute } from "./components/PrivateRouter";
+import { AuthProvider } from "./context/auth-context";
+
+import { HomePage } from "./pages/HomePage";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          {/* <Route path="/calendar">
+            <Calendar />
+          </Route>
+          <PrivateRoute path="/home">
+            <Dashboard />
+          </PrivateRoute>
+          <PrivateRoute path="/calendarPrivate">
+            <CalendarPrivate />
+          </PrivateRoute>
+          <PrivateRoute path="/events_user">
+            <EventsUser />
+          </PrivateRoute>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/events">
+            <GetEvent />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <PrivateRoute path="/profile">
+            <Profile />
+          </PrivateRoute>
+          <PrivateRoute path="/update">
+            <UserUpdate />
+          </PrivateRoute>
+          <PrivateRoute path="/delete">
+            <DeleteAccount />
+          </PrivateRoute>
+          <PrivateRoute path="/add-event">
+            <AddEvent />
+          </PrivateRoute> */}
+        </Switch>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/HomePage.js
@@ -0,0 +1,55 @@
+import React, { Fragment } from 'react';
+import banner from "../img/banner.jpg";
+import AppleIcon from '@material-ui/icons/Apple';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRocket, faCogs } from '@fortawesome/free-solid-svg-icons';
+
+export function HomePage() {
+    return (
+        <Fragment>
+            <div className="home">
+                <img className="w-100"
+                    src={banner}
+                    alt="banner"
+                />
+                <div className="encabezado encabezado d-flex flex-row justify-content-between">
+                <h1>SEEKER</h1>
+                <button className="btn-blue">
+                    ACCEDER
+                </button>
+                </div>
+                <div className="title">
+            
+                    <h1>PLATAFORMA SEEKER</h1>
+            
+                <p>Poderosa plataforma web de busquedas de multiples fuentes, ajustadas a tus necesidades</p>
+                </div> 
+                <div className="footer-home d-flex flex-row justify-content-center">
+                    <div className="footer-column">
+                        <h3>Noticias</h3>
+                        <div className="text-center m-3">
+                        <AppleIcon fontSize="large"/>
+                        </div>
+                        <p>Más de 30.000 noticias a tu disposición y e 
+                        <br/> informacion al día</p>
+                    </div> 
+                    <div className="footer-column">  
+                    <h3>Videos</h3>
+                        <div className="text-center m-3">
+                        <FontAwesomeIcon icon={faRocket} size="2x"/>
+                        </div>
+                        <p>Miles de videos relacionados con tus intereses y todo <br/>lo que puedas buscar</p>
+                    </div> 
+                    <div className="footer-column">
+                        <h3>Artículos</h3>
+                        <div className="text-center m-3">
+                        <FontAwesomeIcon icon={faCogs} size="2x"/>
+                        </div>
+                        <p>Millones de artículos desde los cuales podrás tener<br/> información detallada para tus búsquedas</p>
+                    </div>
+                 </div>
+            </div>
+        </Fragment>
+    )
+}
+
